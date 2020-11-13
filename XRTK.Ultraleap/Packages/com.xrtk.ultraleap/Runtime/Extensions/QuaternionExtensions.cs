98c944073ab20a2990c61ebf51c7c543e7364c04
Dino Fejzagić <dino.f@live.de>
2020-11-13 22:24:15 +0100

    Fix left handed conversion for rotations

--- XRTK.Ultraleap/Packages/com.xrtk.ultraleap/Runtime/Extensions/QuaternionExtensions.cs
+++ XRTK.Ultraleap/Packages/com.xrtk.ultraleap/Runtime/Extensions/QuaternionExtensions.cs
@@ -11,17 +11,35 @@ namespace XRTK.Ultraleap.Extensions
     public static class QuaternionExtensions
     {
         /// <summary>
-        /// Converts a <see cref="Leap.LeapQuaternion"/> object to a <see cref="Quaternion"/> object.
+        /// Converts a <see cref="Leap.LeapQuaternion"/> object to a <see cref="Quaternion"/> object and transforms
+        /// it to Unity's left handed coordinate system.
         /// </summary>
         /// <param name="vector">The <see cref="Leap.LeapQuaternion"/> to convert.</param>
         /// <returns>A <see cref="Quaternion"/> with identical values to the input <see cref="Leap.LeapQuaternion"/>.</returns>
-        public static Quaternion ToQuaternion(this Leap.LeapQuaternion q) => new Quaternion(q.x, q.y, q.z, q.w);
+        public static Quaternion ToLeftHandedUnityQuaternion(this Leap.LeapQuaternion q)
+        {
+            var quaternion = new Quaternion(q.x, q.y, q.z, q.w);
+            var euler = quaternion.eulerAngles;
+            euler.x = -euler.x;
+            euler.y = -euler.y;
+
+            return Quaternion.Euler(euler);
+        }
 
         /// <summary>
-        /// Converts a <see cref="LeapInternal.LEAP_QUATERNION"/> object to a <see cref="Quaternion"/> object.
+        /// Converts a <see cref="LeapInternal.LEAP_QUATERNION"/> object to a <see cref="Quaternion"/> object and transforms
+        /// it to Unity's left handed coordinate system.
         /// </summary>
         /// <param name="vector">The <see cref="LeapInternal.LEAP_QUATERNION"/> to convert.</param>
         /// <returns>A <see cref="Quaternion"/> with identical values to the input <see cref="LeapInternal.LEAP_QUATERNION"/>.</returns>
-        public static Quaternion ToQuaternion(this LeapInternal.LEAP_QUATERNION q) => new Quaternion(q.x, q.y, q.z, q.w);
+        public static Quaternion ToLeftHandedUnityQuaternion(this LeapInternal.LEAP_QUATERNION q)
+        {
+            var quaternion = new Quaternion(q.x, q.y, q.z, q.w);
+            var euler = quaternion.eulerAngles;
+            euler.x = -euler.x;
+            euler.y = -euler.y;
+
+            return Quaternion.Euler(euler);
+        }
     }
 }
