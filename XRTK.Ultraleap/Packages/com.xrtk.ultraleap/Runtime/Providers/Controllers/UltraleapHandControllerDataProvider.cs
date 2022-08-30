0e0fcf1af6c161cf5f5ef65f796e2c2bf2f51004
Stephen Hodgson <hodgson.designs@gmail.com>
2022-08-30 19:08:41 -0400

    renamed a property

--- XRTK.Ultraleap/Packages/com.xrtk.ultraleap/Runtime/Providers/Controllers/UltraleapHandControllerDataProvider.cs
+++ XRTK.Ultraleap/Packages/com.xrtk.ultraleap/Runtime/Providers/Controllers/UltraleapHandControllerDataProvider.cs
@@ -463,7 +463,7 @@ namespace XRTK.Ultraleap.Providers.Controllers
             MixedRealityHandController detectedController;
             try
             {
-                detectedController = new MixedRealityHandController(this, TrackingState.Tracked, handedness, GetControllerMappingProfile(typeof(MixedRealityHandController), handedness));
+                detectedController = new MixedRealityHandController(this, TrackingState.Tracked, handedness, GetControllerProfile(typeof(MixedRealityHandController), handedness));
             }
             catch (Exception e)
             {
